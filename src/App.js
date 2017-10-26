8ab88789f0a84a1c9bcb96eb8d6caf6d8c20cacb
nwre87 <nwre87@gmail.com>
2017-10-26 11:43:17 -1000

    stub out redux, dev-tools install

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import InstaCloneRouter from './routes';
 import { Link } from 'react-router-dom';
+import configureStore from './store/configureStore';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-           
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <InstaCloneRouter/>
+        <Provider store="store">
+          <InstaCloneRouter/>
+        </Provider>
       </div>
     );
   }

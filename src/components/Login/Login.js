510e89bfec08aa3801fdfbbe7b051725f6a7eb3e
nwre87 <nwre87@gmail.com>
2017-10-27 12:41:51 -1000

    add mapStateToProps with actions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header } from '../.';
+import {
+  addEmail,
+  addPassword,
+  submitCreds
+} from '../../actions/user';
 
 class Login extends Component {
 
   static propTypes = {
 
   };
+
+  handleInputChange = ({ target }) => {
+    const { addEmail, addPassword } = this.props;
+    if (target.name === 'email') {
+      // call email action
+      addEmail(target.email);
+    }
+
+    if (target.name === 'password') {
+      // call password action
+      addPassword(target.password);
+    }
+  }
 
   submitCreds = (e) => {
     console.log(e)
@@ -17,8 +35,8 @@ class Login extends Component {
       <div>
         <Header/>
         <form onSubmit={this.submitCreds}>
-          <input type="text" name="email" placeholder="Email" autoFocus/>
-          <input type="text" name="password" placeholder="Password"/>
+          <input type="text" name="email" onChange={this.handleInputChange} placeholder="Email" autoFocus/>
+          <input type="text" name="password" onChange={this.handleInputChange} placeholder="Password"/>
           <input type="submit"/>
         </form>
       </div>
@@ -26,13 +44,26 @@ class Login extends Component {
   }
 }
 
-const mapStateToProps = (state) => (
-  {}
+const mapStateToProps = ({ email, user}) => (
+  {
+    email,
+    user,
+  }
 );
   
-const mapDispatchToProps = () => (
-  {}
-)
+const mapDispatchToProps = (dispatch, props) => (
+  {
+    addEmail(char) {
+      dispatch(addEmail(char))
+    },
+    addPassword(char) {
+      dispatch(addPassword(char))
+    },
+    submitCreds() {
+      dispatch(submitCreds())
+    }
+  }
+);
 
 export default connect(
   mapStateToProps,

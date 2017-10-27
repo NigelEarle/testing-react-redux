b7a47488475eadd0b0b5094eae4b75173f998351
nwre87 <nwre87@gmail.com>
2017-10-27 10:39:29 -1000

    stub out basic user and posts reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,10 +1,22 @@
+import {
+  POSTS_FETCH_SUCCESS,
+  POSTS_FETCH_FAIL
+} from '../actions/posts';
+
 const initialPostsState = {
   error: null,
+  posts: []
 };
 
 const postsReducer = (state = initialPostsState, action = {}) => {
   switch(action.type){
-    case '':
+    case POSTS_FETCH_SUCCESS:
+      return state;
+    case POSTS_FETCH_FAIL:
+      return {
+        ...state,
+        error: 'Something went wrong!',
+      }
     default:
       return state;
   }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,25 @@
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from '../actions/user';
+
 const initialUserState = {
   error: null,
+  user: {
+    email: 'user@email.com',
+    password: '123456'
+  }
 };
 
 const userReducer = (state = initialUserState, action = {}) => {
   switch(action.type){
-    case '':
-      return;
+    case LOGIN_SUCCESS:
+      return state;
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        error: 'Wrong Email or Password. Try again.',
+      }
     default:
       return state;
   }
